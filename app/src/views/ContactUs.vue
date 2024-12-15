727328f501e11d18fca1622e071037fcb61d0f9b
pashkin243 <robert.pass@boldmark.ee>
2024-12-15 21:41:08 +0200

    Contacts page

--- app/src/views/ContactUs.vue
+++ app/src/views/ContactUs.vue
@@ -1,5 +1,13 @@
 <template>
     <div>
         <h2>Contact Us</h2>
+        <p>
+            Our team:
+        </p>
+        <p>Andrei - andrei@postit.com <br>
+            Ann-Marleen - annmarleen@postit.com <br>
+            Robert - robert@postit.com <br>
+            24/7 Phone line: +372654321
+        </p>
     </div>
 </template>
